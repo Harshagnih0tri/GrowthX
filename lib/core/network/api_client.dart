2cc1f313ad4215d866208ac9077c29522f028760
Harshagnih0tri <agnihotri231104@gmail.com>
2026-07-22 05:25:05 +0530

    feat: configure Dio API client

---
+++ lib/core/network/api_client.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+import 'api_constants.dart';
+
+class ApiClient {
+  ApiClient._();
+
+  static final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: ApiConstants.baseUrl,
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      sendTimeout: const Duration(seconds: 30),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    ),
+  );
+}
---
+++ lib/core/network/api_constants.dart
@@ -0,0 +1,12 @@
+class ApiConstants {
+  ApiConstants._();
+
+  // Android Emulator
+  // static const String baseUrl = "http://10.0.2.2:8000/api/v1";
+
+  // Windows / Chrome
+  static const String baseUrl = "http://127.0.0.1:8000/api/v1";
+
+  // Physical Device
+  // static const String baseUrl = "http://YOUR_IP:8000/api/v1";
+}
